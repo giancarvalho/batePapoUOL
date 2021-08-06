552abd84c1eafb3c3a49ee3f2ccb33667f0db1cb
giancarvalho <giandcarvalho@gmail.com>
2021-08-05 22:08:12 -0300

    Reorganized scripts and added comments

--- js/script.js
+++ js/script.js
@@ -1,33 +1,70 @@
+const URL_PARTICIPANTS =
+  "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants";
+const URL_MESSAGES =
+  "https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages";
+const URL_STATUS =
+  "https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/status";
 const messageList = document.querySelector(".chat-box > ul");
+let username;
+const userMessage = {};
 
-function toggleMenu() {
-  const sideMenu = document.querySelector(".side-menu");
-  const overlay = document.querySelector(".overlay");
+getUsername();
 
-  sideMenu.classList.toggle("active");
-  overlay.classList.toggle("active");
-}
+// gets and validates username
+function getUsername() {
+  username = prompt("Qual o seu nome?");
 
-function selectElement(element) {
-  const previousSelected = document.querySelector(".contact .active");
-
-  if (previousSelected !== element && previousSelected !== null) {
-    previousSelected.classList.remove("active");
+  // send to server user
+  if (username !== "" && username !== null) {
+    sendUser(username);
+    getMessages();
+  } else {
+    getUsername();
   }
-  element.classList.add("active");
 }
 
+// send username to api
+function sendUser(username) {
+  let promise = axios.post(URL_PARTICIPANTS, { name: username });
+
+  promise.catch(handleError);
+}
+
+function handleError(error) {
+  if (error.response.status === 400) {
+    alert("Nome nao disponivel");
+    getUsername();
+  }
+}
+
+// get messages from server
 function getMessages() {
-  let promise = axios.get(
-    "https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages"
-  );
+  let promise = axios.get(URL_MESSAGES);
 
   promise.then(renderMessages);
-  promise.catch(alertError);
+  //promise.catch(handleError);
 }
 
+// get message details from current user, not yet working
+function sendMessage() {
+  userMessage.from = username;
+  userMessage.to = document.querySelector(".contact .selected").innerText;
+  userMessage.text = document.querySelector(".bottom-bar input").value;
+  userMessage.type = document.querySelector(".visibility .selected").innerText;
+
+  let promise = axios.post(URL_MESSAGES, userMessage);
+}
+
+// makes sure the messages are up to date and user is online, sendUser is the wrong function
+function requestInterval(username) {
+  setInterval(getMessages, 3000);
+  setInterval(sendUser, 3000, username);
+}
+
+// render messages scripts
 function renderMessages(response) {
   const messages = response.data;
+  messageList.innerHTML = "";
 
   for (let i = 0; messages.length > i; i++) {
     if (messages[i].type === "status") {
@@ -69,6 +106,21 @@ function renderMessage(message) {
 </li>`;
 }
 
-function alertError(error) {
-  console.log(error);
+// animation page
+
+function toggleMenu() {
+  const sideMenu = document.querySelector(".side-menu");
+  const overlay = document.querySelector(".overlay");
+
+  sideMenu.classList.toggle("active");
+  overlay.classList.toggle("active");
+}
+
+function selectElement(element) {
+  const previousSelected = document.querySelector(".contact .selected");
+
+  if (previousSelected !== element && previousSelected !== null) {
+    previousSelected.classList.remove("selected");
+  }
+  element.classList.add("selected");
 }
